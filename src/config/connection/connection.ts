7f6ada8b667b753e58de4f1b6601d995cdd8d01f
Moises Escobar Martinez <moisesescobarmartinez1@gmail.com>
2023-12-21 22:22:31 -0600

    fixed: error sequlize

--- src/config/connection/connection.ts
+++ src/config/connection/connection.ts
@@ -2,7 +2,7 @@ import { Sequelize } from 'sequelize';
 import config from '../env/index';
 
 const ssl = (config.NODE_ENV=='production')?"?sslmode=require":"";
-const sequelize = new Sequelize(config.DATABASE_POSTGRES.DATABASE_URL+ssl,{
+const sequelize = new Sequelize(config.DATABASE_POSTGRES.DATABASE_URL+ssl,null,null{
   logging: false,
 });
 export default sequelize;
