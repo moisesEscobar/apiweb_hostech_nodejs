105ce0591c1bfb554d71c4359585830e41660467
Moises Escobar Martinez <moisesescobarmartinez1@gmail.com>
2023-12-21 22:52:54 -0600

    fixed: change config sequalize

--- src/config/connection/connection.ts
+++ src/config/connection/connection.ts
@@ -1,9 +1,16 @@
 import { Sequelize } from 'sequelize';
 import config from '../env/index';
 
-const ssl = (config.NODE_ENV=='production')?"?sslmode=require":"";
-const sequelize = new Sequelize(config.DATABASE_POSTGRES.DATABASE_URL+ssl,{
+//const ssl = (config.NODE_ENV=='production')?"?sslmode=require":"";
+/* const sequelize = new Sequelize(config.DATABASE_POSTGRES.DATABASE_URL+ssl,{
   dialect: 'postgres',
   logging: false
+}); */
+const ssl= (config.NODE_ENV=='production')?true:false;
+const sequelize = new Sequelize('verceldb', 'default', 'Ug4aSABKVc7f', {
+  host: 'ep-sparkling-tree-24352510-pooler.us-east-1.postgres.vercel-storage.com',
+  dialect: 'postgres',
+  logging: false,
+  dialectOptions: { ssl: ssl}
 });
 export default sequelize;
--- src/config/env/index.ts
+++ src/config/env/index.ts
@@ -6,9 +6,13 @@ interface IConfig {
     NODE_ENV: string,
     PORT: string | number;
     DATABASE_POSTGRES:{
-        DATABASE_URL: string
+        DATABASE_URL: string,
+        POSTGRES_USER: string,
+        POSTGRES_HOST: string,
+        POSTGRES_PASSWORD: string,
+        POSTGRES_DATABASE: string
     },
-    SECRET: string
+    SECRET: string,
 }
 const NODE_ENV: string = process.env.NODE_ENV || 'production';
 
@@ -17,16 +21,25 @@ const development: IConfig = {
     NODE_ENV: process.env.NODE_ENV,
     PORT: process.env.PORT || 3000,
     DATABASE_POSTGRES:{
-        DATABASE_URL:process.env.POSTGRES_URL
+        DATABASE_URL:process.env.POSTGRES_URL,
+        POSTGRES_USER: process.env.POSTGRES_USER,
+        POSTGRES_HOST: process.env.POSTGRES_HOST,
+        POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
+        POSTGRES_DATABASE: process.env.POSTGRES_DATABASE
     },
-    SECRET:process.env.SECRET
+    SECRET:process.env.SECRET,
+    
 };
 const production: IConfig = {
     HOST: process.env.HOST,
     NODE_ENV: process.env.NODE_ENV,
     PORT: process.env.PORT || 3000,
     DATABASE_POSTGRES:{
-        DATABASE_URL:process.env.POSTGRES_URL
+        DATABASE_URL:process.env.POSTGRES_URL,
+        POSTGRES_USER: process.env.POSTGRES_USER,
+        POSTGRES_HOST: process.env.POSTGRES_HOST,
+        POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
+        POSTGRES_DATABASE: process.env.POSTGRES_DATABASE
     },
     SECRET:process.env.SECRET
 };
