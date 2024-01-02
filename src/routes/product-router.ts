30dcab95cfffbdffed04a866c710473d3f9ea359
Moises Escobar Martinez <moisesescobarmartinez1@gmail.com>
2024-01-01 23:35:02 -0600

    updated: add test and fixed errors and add file in products

--- src/routes/product-router.ts
+++ src/routes/product-router.ts
@@ -1,11 +1,15 @@
+//import multer from "multer";
+//import path from "path";
+import uploadStoarge from '../config/storage/multer';
 import { ProductComponent } from '../components';
 import { Router } from 'express';
+//const uploadStoarge = multer({ dest: path.join(__dirname, '/') });
 
 const router: Router = Router();
 router.get('/find_all', ProductComponent.findAll);
 router.get('/find_one/:id', ProductComponent.findOne);
 router.get('/search', ProductComponent.search);
-router.post('/create', ProductComponent.create);
+router.post('/create', uploadStoarge.single('file'),ProductComponent.create);
 router.put('/update/:id', ProductComponent.update);
 router.delete('/remove/:id', ProductComponent.remove);
 router.put('/restore/:id', ProductComponent.restore);
